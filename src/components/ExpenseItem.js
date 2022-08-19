db18b3da7ac74fd672a61b5169b53f88990abd34
whitebsm93 <whitebsm93@naver.com>
2022-08-20 04:12:27 +0900

    컴포넌트 폴더 구분, card 컴포넌트

--- src/components/ExpenseItem.js
+++
@@ -1,5 +0,0 @@
-const ExpenseItem = () => {
-    return <h2>Expense Item!</h2>
-}
-
-expect default ExpenseItem
---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,16 @@
+import Card from '../UI/Card';
+import ExpenseItem from './ExpenseItem';
+
+import './Expenses.css'
+
+const Expenses = (props) => {  
+    return (
+        <Card className='expenses'>
+            {props.items.map((item) => {
+                return <ExpenseItem title={item.title} amount={item.amount} date = {item.date}/>
+            })}
+        </Card>
+    )
+}
+
+export default Expenses
